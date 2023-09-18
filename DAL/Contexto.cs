db6a0e343e17df77adb99fe4fb97e3f267d2af1c
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-09-17 23:24:53 -0400

    Añadiendo el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketsApp.Models;
+
+namespace TicketsApp.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options)
+            : base(options) { }
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Prioridades> Prioridades { get; set; }
+        public DbSet<Tickets> Tickets { get; set; }
+        public DbSet<Sistemas> Sistemas { get; set; }
+    }
+}
