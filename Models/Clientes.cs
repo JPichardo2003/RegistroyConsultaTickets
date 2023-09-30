b0289755b8409b7119ab624e2f4b7c10cfa7298a
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-09-30 15:21:56 -0400

    Añadiendo un input select para filtrar por diferentes campos

--- Models/Clientes.cs
+++ Models/Clientes.cs
@@ -12,7 +12,7 @@ namespace TicketsApp.Models
         [StringLength(10, ErrorMessage = "No Puede Exceder los 10 Caracteres")]
         [Required(ErrorMessage = "Campo Obligatorio")]
         public string? Telefono { get; set; }
-        [StringLength(15, ErrorMessage = "No Puede Exceder los 12 Caracteres")]
+        [StringLength(15, ErrorMessage = "No Puede Exceder los 15 Caracteres")]
         [Required(ErrorMessage = "Campo Obligatorio")]
         public string? Celular { get; set; }
         [StringLength(9, ErrorMessage = "No Puede Exceder los 9 Caracteres")]
